b0f47894cb8c01035233bccd8b0fc90e687b2b89
Kevin <ckevin2797@gmail.com>
2025-07-29 10:21:50 +0700

    Initial Commit

---
+++ SwiftUIScrollView/SwiftUIScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIScrollViewApp.swift
+//  SwiftUIScrollView
+//
+//  Created by Kevin on 29/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
