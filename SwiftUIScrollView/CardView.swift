944d40df954da2871beb639eced3308b2956125b
Kevin <ckevin2797@gmail.com>
2025-10-10 07:27:24 +0700

    Fixed font

--- SwiftUIScrollView/CardView.swift
+++ SwiftUIScrollView/CardView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct CardView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image("swiftui-button")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+
+        VStack(alignment: .leading) {
+            Text("SwiftUI")
+                .font(.largeTitle)
+        }
     }
 }
 
