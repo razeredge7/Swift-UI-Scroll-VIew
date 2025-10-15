4ed9597fc4b1b80de86414b5b16236ab59827318
Kevin <ckevin2797@gmail.com>
2025-10-15 08:02:10 +0700

    Resized width, grouped several items

--- SwiftUIScrollView/CardView.swift
+++ SwiftUIScrollView/CardView.swift
@@ -8,11 +8,47 @@
 import SwiftUI
 
 struct CardView: View {
+    var image: String
+    var category: String
+    var heading: String
+    var author: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        //VStack to wrap text and image
+        VStack{
+            Image(image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            HStack{
+                //HStack to give spacer
+                VStack(alignment: .leading) {
+                    Text(category)
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                    Text(heading)
+                        .font(.title)
+                        .fontWeight(.black)
+                        .foregroundColor(.primary)
+                        .lineLimit(3)
+                    Text("Written by \(author)".uppercased())
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+                //leading spacer
+                Spacer()
+            }.minimumScaleFactor(0.5)
+            .padding()
+        }
+        
+        .cornerRadius(10)
+        .overlay(RoundedRectangle(cornerRadius: 10)
+            .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.1), lineWidth: 1)
+        )
+        .padding([.top, .horizontal])
     }
 }
 
-#Preview {
-    CardView()
-}
+//#Preview {
+//    CardView(image: "swiftui-button", category: "SwiftUI", heading: "Drawing a border with rounded corners", author: "Simon Ng")
+//}
--- SwiftUIScrollView/ContentView.swift
+++ SwiftUIScrollView/ContentView.swift
@@ -9,13 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading){
+            Text("MONDAY, AUG 20")
+                .foregroundColor(.secondary)
+                .font(.title3)
+            Text("Your Reading")
+                .fontWeight(.black)
+            
+                
+            ScrollView(.horizontal, showsIndicators: false){// or vertical no need to define
+                HStack {
+                    Group{
+                        CardView(image: "swiftui-button",
+                                 category: "SwiftUI",
+                                 heading: "Drawing a border with Rounded Corners",
+                                 author: "Simon Ng")
+                        
+                        CardView(image: "macos-programming",
+                                 category: "macOS", heading: "Building a Simple Editing App",
+                                 author: "Gabriel Theodoropoulos")
+                        
+                        CardView(image: "flutter-app",
+                                 category: "Flutter",
+                                 heading: "Building a complex layout with Flutter",
+                                 author: "Lawrence Tan")
+                        
+                        CardView(image: "natural-language-api",
+                                 category: "iOS",
+                                 heading: "What's new in Natural Language API",
+                                 author: "Sai Kambampati")
+                        
+                    }
+                    .frame(width:300)
+                }
+//                .padding()
+                Spacer()
+            }
         }
-        .padding()
+        .font(.largeTitle)
+        .padding(20)
+//        Spacer()
     }
 }
 
